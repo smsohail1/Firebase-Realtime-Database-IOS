5c94ce7794d8f54450983a0166064c9a431126a7
Abdul Ahad <abdulahad@Abduls-iMac.local>
2017-07-18 06:23:43 -0700

    Initial Commit

---
+++ Firebase Realtime database IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firebase Realtime database IOS
+//
+//  Created by Abdul Ahad on 7/18/17.
+//  Copyright © 2017 plash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Firebase Realtime database IOSTests/Firebase_Realtime_database_IOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Firebase_Realtime_database_IOSTests.swift
+//  Firebase Realtime database IOSTests
+//
+//  Created by Abdul Ahad on 7/18/17.
+//  Copyright © 2017 plash. All rights reserved.
+//
+
+import XCTest
+@testable import Firebase_Realtime_database_IOS
+
+class Firebase_Realtime_database_IOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
